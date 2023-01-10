6b623b02c51c9fdce02a88b9aed54e70bce33342
Naozumi Isayama <passion.qdo@gmail.com>
2023-01-10 21:51:22 +0900

    #9 データ表示の修正

--- lib/infrastracture/issue_list.dart
+++ lib/infrastracture/issue_list.dart
@@ -4,7 +4,7 @@ import 'package:fregov_app/models/issue_model.dart';
 
 final issueListProvider = StreamProvider.autoDispose((ref) {
   CollectionReference ref = FirebaseFirestore.instance.collection('issues');
-  return ref.snapshots().map((snapshot) => snapshot.docs
+  return ref.orderBy('updateTime', descending: true).snapshots().map((snapshot) => snapshot.docs
       .map((doc) => doc.data()! as Map<String, dynamic>)
       .map((data) => Issue(
         cityName: data['cityName'],
